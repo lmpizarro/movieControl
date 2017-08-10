8ee24f0a17a35cca39c59b26bcd2ced682eb281c
Luis María Pizarro <lmpizarro@gmail.com>
2017-08-10 18:11:22 -0300

    init iXml interface modif

--- src/iXml/piXml.py
+++ src/iXml/piXml.py
@@ -1,72 +1,104 @@
 import xml.etree.ElementTree as etree
 import xmltodict
 
-
 class Ixml(object):
     def __init__(self, xmlFile):
         self.xmlFile = xmlFile
         self.tree = etree.parse(self.xmlFile)
         self.root = self.tree.getroot()
 
-        self.setProject()
-        self.setIxml_Version()
-        self.setScene()
-        self.setTake()
-        self.setTape()
-        self.setCircled()
-        self.setNo_Good()
-        self.setFalse_Start()
-        self.setWild_Track()
-        self.STR = etree.tostring(self.root)
-
-    def setProject(self):
-        self.PROJECT = self.root.findall("PROJECT")[0].text
-
-    def setIxml_Version(self):
-        self.IXML_VERSION = self.root.findall("IXML_VERSION")[0].text
-
-    def setScene(self):
-        self.SCENE = self.root.findall("SCENE")[0].text
-
-    def setTake(self):
-        self.TAKE = self.root.findall("TAKE")[0].text
+        self.toDict()
 
 
-    def setTape(self):
-        self.TAPE = self.root.findall("TAPE")[0].text
+    def getProject(self):
+        return self.dict_["BWFXML"]["PROJECT"]
 
-    def setCircled(self):
-        self.CIRCLED = self.root.findall("CIRCLED")[0].text
+    def getIxml_Version(self):
+        return self.dict_["BWFXML"]["IXML_VERSION"]
 
+    def getScene(self):
+        return self.dict_["BWFXML"]["SCENE"]
 
-    def setNo_Good(self):
-        self.NO_GOOD = self.root.findall("NO_GOOD")[0].text
+    def getTake(self):
+        return self.dict_["BWFXML"]["TAKE"]
 
-    def setFalse_Start(self):
-        self.FALSE_START = self.root.findall("FALSE_START")[0].text
+    def getTape(self):
+        return self.dict_["BWFXML"]["TAPE"]
 
-    def setWild_Track(self):
-        self.WILD_TRACK = self.root.findall("WILD_TRACK")[0].text
+    def getCircled(self):
+        return self.dict_["BWFXML"]["CIRCLED"]
+
+    def getNo_Good(self):
+        return self.dict_["BWFXML"]["NO_GOOD"]
+
+    def getFalse_Start(self):
+        return self.dict_["BWFXML"]["FALSE_START"]
+
+    def getWild_Track(self):
+        return self.dict_["BWFXML"]["WILD_TRACK"]
+
+    def getFile_Uid(self):
+        return self.dict_["BWFXML"]["FILE_UID"]
+
 
     def to_dict(self):
         from json import loads, dumps
         return loads(dumps((xmltodict.parse(self.STR))))
 
 
-    def getDict (self):
+    def toDict (self):
+
         dict_ = {}
+        dict_[self.root.tag] = {}
 
-        dict_["PROJECT"] = self.PROJECT
-        dict_["IXML_VERSION"] = self.IXML_VERSION
-        dict_["SCENE"] = self.SCENE
-        dict_["TAKE"] = self.TAKE
-        dict_["TAPE"] = self.TAPE
-        dict_["CIRCLED"] = self.CIRCLED
-        dict_["NO_GOOD"] = self.NO_GOOD
-        dict_["FALSE_START"] = self.FALSE_START
-        dict_["WILD_TRACK"] = self.WILD_TRACK
+        root = self.root
 
-        return dict_
+        for child in root:
+            if len(child) > 0:
+                dict_[root.tag][child.tag] = {} 
+
+                if child.tag == "SPEED":
+                    for c in child:
+                        dict_[root.tag][child.tag][c.tag] = c.text 
+
+                if child.tag == "BEXT":
+                    for c in child:
+                        dict_[root.tag][child.tag][c.tag] = c.text 
+
+                if child.tag == "HISTORY":
+                    for c in child:
+                        dict_[root.tag][child.tag][c.tag] = c.text 
+
+                if child.tag == "FILE_SET":
+                    for c in child:
+                        dict_[root.tag][child.tag][c.tag] = c.text 
+
+                if child.tag == "TRACK_LIST":
+                    dict_[root.tag][child.tag] = []
+                    for c in child:
+                        if len(c) > 0:
+                            a = {}
+                            for s in c:
+                                a[s.tag] = s.text
+                            dict_[root.tag][child.tag].append(a)
+
+                if child.tag == "SYNC_POINT_LIST":
+                    dict_[root.tag][child.tag] = []
+                    for c in child:
+                        if len(c) > 0:
+                            a = {}
+                            for s in c:
+                                a[s.tag] = s.text
+                            dict_[root.tag][child.tag].append(a)
+    
+            else:
+               dict_[root.tag][child.tag] = child.text
+
+
+        self.dict_ =  dict_ 
+    
+    def getDict(self):
+        return self.dict_
 
 
 if __name__ == "__main__":
@@ -74,67 +106,20 @@ if __name__ == "__main__":
     fileName = 'testixml.xml'
     ixml = Ixml (fileName)
 
-    #print (ixml.getDict())
+    dict_ = ixml.getDict()
 
+    print ("NO_GOOD: ", ixml.getNo_Good())
+    print ("IXML_VERSION: ", ixml.getIxml_Version())
+    print ("PROJECT: ", ixml.getProject())
+    print ("SCENE: ", ixml.getScene())
+    print ("TAKE: ", ixml.getTake())
+    print ("TAPE: ",  ixml.getTape())
+    print ("CIRCLED: ", ixml.getCircled())
+    print ("NO_GOOD: ", ixml.getNo_Good())
+    print ("FALSE_START: ", ixml.getFalse_Start())
+    print ("WILD_TRACK: ", ixml.getWild_Track())
+    print ("FILE_UID: ", ixml.getFile_Uid())
 
-    tree = etree.parse(fileName )
-    root = tree.getroot()
-
-    dict_ = {}
-    dict_[root.tag] = {}
-
-
-    for child in root:
-        if len(child) > 0:
-            dict_[root.tag][child.tag] = {} 
-
-
-            if child.tag == "SPEED":
-                for c in child:
-                    dict_[root.tag][child.tag][c.tag] = c.text 
-
-            if child.tag == "BEXT":
-                for c in child:
-                    dict_[root.tag][child.tag][c.tag] = c.text 
-
-
-            if child.tag == "HISTORY":
-                for c in child:
-                    dict_[root.tag][child.tag][c.tag] = c.text 
-
-
-            if child.tag == "FILE_SET":
-                for c in child:
-                    dict_[root.tag][child.tag][c.tag] = c.text 
-
-
-            if child.tag == "TRACK_LIST":
-                dict_[root.tag][child.tag] = []
-                for c in child:
-                    if len(c) > 0:
-                        a = {}
-                        for s in c:
-                            a[s.tag] = s.text
-                        dict_[root.tag][child.tag].append(a)
-
-
-
-            if child.tag == "SYNC_POINT_LIST":
-                dict_[root.tag][child.tag] = []
-                for c in child:
-                    if len(c) > 0:
-                        a = {}
-                        for s in c:
-                            a[s.tag] = s.text
-                        dict_[root.tag][child.tag].append(a)
-
-
- 
-
-
-        else:
-           dict_[root.tag][child.tag] = child.text
-
-
-    print (dict_)
+    for k in dict_["BWFXML"]["SPEED"]:
+        print ("SPEED ", k, " " , dict_["BWFXML"]["SPEED"][k])
 
